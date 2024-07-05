cd690e7a5243b427329dac8203c0cb2af7752d71
Paul Greetham <pgreetham@protonmail.com>
2024-07-05 15:39:12 +0200

    as we were

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import './App.scss';
+import Header from './components/header/Header';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to Natours</h1>
-
-      </header>
-    </div>
+    <Header />
   );
 }
 
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Header.scss';
+import logo from '../../assets/images/logo-white.png';
+
+const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <div className="logo-box">
+        <img src={logo} alt="logo" className="logo" />
+      </div>
+      <div className="text-box">
+        <h1 className="heading-primary">
+          <span className="heading-main">Outdoors</span>
+          <span className="heading-sub">is where life happens</span>
+        </h1>
+        <a href="#" className="btn btn-white btn-animated">Discover our tours</a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
