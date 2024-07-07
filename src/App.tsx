b058a8ffda7932f75dec52cf0aee221cc380731b
Paul Greetham <pgreetham@protonmail.com>
2024-07-07 11:33:27 +0200

    about section adeed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.scss';
 import Header from './components/header/Header';
+import About from './components/about/About';
 
 const App: React.FC = () => {
   return (
-    <Header />
+    <>
+      <Header />
+      <About />
+    </>
   );
 }
 
---
+++ src/components/about/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.scss';
+
+const About: React.FC = () => {
+  return (
+    <main className="about">
+      <h2>Exciting tours for adventurous people</h2>
+    </main>
+  );
+};
+
+export default About;
