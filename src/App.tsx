3fc5fabf2439e3513b7abc95eab57d4cc9cdea08
Paul Greetham <pgreetham@protonmail.com>
2024-07-07 15:10:36 +0200

    small changs to about section

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.scss';
 import Header from './components/header/Header';
 import About from './components/about/About';
+import Features from './components/features/Features';
 
 const App: React.FC = () => {
   return (
     <>
       <Header />
       <About />
+      <Features />
     </>
   );
 }
---
+++ src/components/features/Features.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Features.scss';
+
+const Features: React.FC = () => {
+  return (
+    <section className="features">
+      hello features
+    </section>
+  );
+};
+
+export default Features;
